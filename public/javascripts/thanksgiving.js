74037113b7cb93d468c6912fc6b63eff91f1cf3b
Luca Virgilio <luca.virgilio95@gmail.com>
2020-09-08 18:35:10 +0200

    Fix txId visualization and button

--- public/javascripts/thanksgiving.js
+++ public/javascripts/thanksgiving.js
@@ -4,9 +4,13 @@ let count = 20;
 
 async function countDown() {
     try {
-        if(count == 20) {
+        if (count == 20) {
+            $(function () {
+                $('[data-toggle="popover"]').popover()
+              })
             const txId = sessionStorage.getItem("txId");
-            document.getElementById("txId").innerText = `Il tuo transaction Id è ${txId}`;
+            document.getElementById("txId-value").innerHTML = `${txId}`;
+
         }
         if (count > 0) {
             console.log('timer ', count);
@@ -14,13 +18,26 @@ async function countDown() {
             await timer();
             countDown();
         } else {
+            sessionStorage.removeItem('txId');
             window.location.href = "/index.html";
         }
     } catch (error) {
         console.log(error);
     }
 }
-
+const copyId = _ => {
+    console.log("copy");
+    const txId = sessionStorage.getItem("txId");
+    let dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.value = txId;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    setTimeout(function () {
+        $('[data-toggle="popover"]').popover('hide');
+    }, 2000);
+}
 async function timer() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
